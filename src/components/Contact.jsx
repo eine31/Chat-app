2c2d34152f9c888b979c9fd668a232c4802c893f
Navaloniaina Rakotovao <tsiresy@gmail.com>
2019-10-09 05:30:48 +0200

    premier commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Contacts.css'
+
+function Contact(props) {
+    return (
+      <div className="Contact">
+        <img src={props.avatar} className = "avatar" alt = "avatar"/> 
+        <div className="status">
+          <h4 className="name">{props.name}</h4>
+          <div className="status-bloc">
+            <p className={props.online ? "status-online" : "status-offline"}></p>
+            <p className="status-text">{props.online ? "Online" : "Offline"}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+     
+export default Contact;
+
+
